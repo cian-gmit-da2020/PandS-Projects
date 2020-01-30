781520039c1f52874f548069e208547054d203b1
cian-gmit-da2020 <60194136+cian-gmit-da2020@users.noreply.github.com>
2020-01-30 21:10:22 +0000

    Add files via upload
    updated, simpler code

--- week3_task.py
+++ week3_task.py
@@ -7,24 +7,6 @@ string = input("Enter a sentence here: ")
 # reverses the input
 revstring = string[len(string)::-1]
 
-# creates empty list
-secondletters = []
-
-# loops through each letter in the reversed string
-for i in range(0, len(revstring)):
-
-	# checks if the letter is a space character and adds spaces to list
-	if revstring[i] == " ":
-		secondletters.append(revstring[i])
-
-	# checks for even indexes and adds to list
-	elif i % 2 == 0:
-		secondletters.append(revstring[i])
-		
-	# Skips odd indexes
-	else:
-		continue
-
-# joins the list together into new string and prints
-print("".join(secondletters))
+# prints every second char of revstring
+print(revstring[0::2])
 
