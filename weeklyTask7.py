4e5e372bcb595c1913023f4f98f523d103e51046
Cian <G00387860@gmit.ie>
2020-02-25 19:25:51 +0000

    Weekly task7 and mobydick text file

---
+++ weeklyTask7.py
@@ -0,0 +1,25 @@
+# Cian Hogan Week 7 task
+# import sys module
+
+import sys
+
+# convert command line argument to string
+fname = str(sys.argv[-1])
+
+# open file in read mode
+f = open(fname, 'r')
+
+# convert the file to a list of lines
+lines = f.readlines()
+# set iterator to 0
+count = 0
+# for line in list lines check each letter
+for line in lines:
+	# for each character in the line string check if it is 'e'
+	for char in line:
+	# if its e increment count by 1
+		if char == "e": # or char == "E":
+			count += 1
+
+# print results script
+print("There are", count, "e's in", fname)
